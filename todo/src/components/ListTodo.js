889afda54ab33eae6e0942d22906fabd9ae57cfa
ellie <ellie.tms1371@gmail.com>
2021-12-29 22:06:01 +0000

    add: Todolist

--- todo/src/components/ListTodo.js
+++ todo/src/components/ListTodo.js
@@ -1,1 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
+
+const ListTodos = ({ listTodo }) => {
+  let page;
+  if (listTodo.length !== 0) {
+    page = (
+      <div>
+        <h3> My Todolist</h3>
+        {listTodo.map((eachTodo, index) => (
+          <div key={index}>
+            <input type="checkbox" id="todoList" name={eachTodo}>
+              <label for={eachTodo}> {eachTodo} </label>
+            </input>
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    page = (
+      <>
+        <h3>Please add something for your todo list!</h3>
+      </>
+    );
+  }
+  return <>{page}</>;
+};
+
+export default ListTodos;
--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
+import ListTodo from "./ListTodo";
 
 const TodoList = () => {
   const [todo, setTodo] = useState("");
   const [listTodo, setListTodo] = useState([]);
+
   const handleTodo = (event) => {
     setTodo(event.target.value);
   };
+
   const submitTodo = (event) => {
     event.preventDefault();
     setListTodo([...listTodo, todo]);
@@ -20,18 +23,7 @@ const TodoList = () => {
           onChange={(event) => handleTodo(event)}
         />
         <button type="submit">submit</button>
-        {listTodo.length !== 0 && (
-          <div>
-            <h3> My Todolist</h3>
-            {listTodo.map((eachTodo, index) => (
-              <div key={index}>
-                <input type="checkbox" id="todoList" name={eachTodo}>
-                  <label for={eachTodo}> {eachTodo} </label>
-                </input>
-              </div>
-            ))}
-          </div>
-        )}
+        <ListTodo listTodo={listTodo} />
       </form>
     </>
   );
