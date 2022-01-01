cf967f61ab5ecadec13740a9d55a31ff2a26b583
eli <ellie.tms1371@gmail.com>
2022-01-01 17:48:53 +0000

    add: functions

--- todo/src/components/ListOfTodos.js
+++ todo/src/components/ListOfTodos.js
@@ -1,23 +1,27 @@
 import React from "react";
 import Todo from "./EachTodo";
 
-const listOfTodos = ({ listOfTodos }) => {
+const listOfTodos = ({ listOfTodos, handleDelete, handleEdit }) => {
   let page;
   if (listOfTodos.length !== 0) {
     page = (
-      <ul>
+      <ul className="no-bullet-point">
         {listOfTodos.map((eachTodo) => (
           <li key={eachTodo.id}>
-            <Todo eachTodo={eachTodo} />
+            <Todo
+              eachTodo={eachTodo}
+              handleDelete={(eachTodo) => handleDelete(eachTodo)}
+              handleEdit={handleEdit}
+            />
           </li>
         ))}
       </ul>
     );
   } else {
     page = (
-      <>
+      <div>
         <h3>Please add something for your todo list!</h3>
-      </>
+      </div>
     );
   }
   return <>{page}</>;
