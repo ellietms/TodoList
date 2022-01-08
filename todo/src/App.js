057907030d1651566a3f0b94d83d5bb3a74b4c8d
Daniel Wagner-Hall <dawagner@gmail.com>
2022-01-08 22:30:25 +0000

    Misc feedback

--- todo/src/App.js
+++ todo/src/App.js
@@ -5,9 +5,12 @@ import ListOfTodos from "./components/ListOfTodos";
 import { nanoid } from "nanoid";
 
 function App() {
+  // What is this state for? Could you give it a more descriptive name showing what it means?
   const [todo, setTodo] = useState("");
   const [listOfTodos, setListOfTodos] = useState([]);
+  // This state is never read? Why does it exist?
   const [editAvailable, setEditAvailable] = useState(false);
+  // It looks like this is always the same as listOfTodos.length? Why do you need to store it separately? It feels like this just means the two can go out of sync if you forget to update them both together?
   const [count, setCount] = useState(0);
 
   const handleTodo = (event) => {
@@ -18,6 +21,7 @@ function App() {
     event.preventDefault();
     setListOfTodos([
       ...listOfTodos,
+      // What's a nanoid? What are you using it for?
       { text: todo, edit: false, id: nanoid(), sort: count },
     ]);
     setCount(count + 1);
@@ -25,12 +29,16 @@ function App() {
   };
 
   const handleDelete = (event) => {
+    // Does this mean if you have two of the same todo and delete one, both will be deleted?
+    // Is this what you expect?
+    // I would have assumed the IDs were to avoid this?
     let filteredList = listOfTodos.filter((todo) => todo.text !== event.text);
     setListOfTodos(filteredList);
     setTodo("");
   };
 
   const handleEdit = (event) => {
+    // Same as delete - does this mean editing one will edit the first matching one, even if that's not the one you clicked on?
     let currentTodo = listOfTodos.find((todo) => todo.text === event.text);
     if (currentTodo.edit == true) {
       setEditAvailable(false);
--- todo/src/components/HomePage.js
+++ todo/src/components/HomePage.js
@@ -1,6 +1,8 @@
 /* eslint-disable */
 import React, { useState } from "react";
 
+// I would kind of expect a HomePage to be a whole page, not just a single text box and form - maybe there's a better name for this component?
+// It's not particularly clear what each of these props is for. What's a todo to be handled? Does it mean a new todo? What's the btn? What does the btn do? What's a todo? If the point of this component is to show a list of todos, it seems strange to have a prop which is a single todo?
 const HomePage = ({ todo, handleTodo, handleBtn }) => {
   const page = (
     <div>
@@ -11,6 +13,7 @@ const HomePage = ({ todo, handleTodo, handleBtn }) => {
           value={todo}
           onChange={(event) => handleTodo(event)}
         />
+        {/* submit doesn't seem like the verb I'd associate with this action - can you think of a better word? */}
         <button className="button-submit" type="submit" onClick={handleBtn}>
           submit
         </button>
